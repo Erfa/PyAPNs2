c3e0c2b7f1e721134d998f2e7d32a1910c1e30e7
Erik Fahlén <erik@feomedia.com>
2016-11-28 10:11:20 +0100

    Make it possible to set a path to your CA certificates.

--- apns2/client.py
+++ apns2/client.py
@@ -14,10 +14,10 @@ class NotificationPriority(Enum):
 
 
 class APNsClient(object):
-    def __init__(self, cert_file, use_sandbox=False, use_alternative_port=False, proto=None, json_encoder=None):
+    def __init__(self, cert_file, use_sandbox=False, use_alternative_port=False, proto=None, json_encoder=None, ca_certs=None):
         server = 'api.development.push.apple.com' if use_sandbox else 'api.push.apple.com'
         port = 2197 if use_alternative_port else 443
-        ssl_context = init_context()
+        ssl_context = init_context(cert_path=ca_certs)
         ssl_context.load_cert_chain(cert_file)
         self.__connection = HTTP20Connection(server, port, ssl_context=ssl_context, force_proto=proto or 'h2')
         self.__json_encoder = json_encoder
